f24f6a48af32afb3252ab1a48c20710ce0174242
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-02-28 02:39:36 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// function program1() {
+//   console.log("from 1");
+// }
+
+// function program2() {
+//   console.log("from 2");
+// }
+
+// function program3() {
+//   console.log("from 2");
+// }
+
+// program1();
+// program2();
+// program3();
