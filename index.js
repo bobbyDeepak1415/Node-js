71279cf3d72bd01041a2dc7ec8af75873919221e
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-04-02 02:28:30 +0530

    setup done

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 
+const obj1={
+    fName:"Bobby",
+    lName:"Deepak"
+}
+
+function printName(){
+    console.log(this.fName+" "+this.lName)
+}
+
+
+printName.call(obj1)
+// console.log("hello")
