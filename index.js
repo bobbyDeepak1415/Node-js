7c143904391a76a3274f761745785ca34a54d502
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-04-16 01:17:34 +0530

    setup done

--- index.js
+++ index.js
@@ -5,7 +5,18 @@ let arr=[3,3,4,5,6,7,8,8]
 let obj={}
 
 for(let i=0;i<arr.length;i++){
-    
+    let item=arr[i]
+    obj[item]=(obj[item] || 0)+1
+
 }
 
-...
+
+let result=[]
+
+for(let key in obj){
+    if(obj[key]===1){
+        result.push(Number[key])
+    }
+}
+
+console.log(result)
