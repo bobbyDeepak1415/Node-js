1c6ae3dc09b0cf8c27ef63bd18067f47b02c8fcc
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-04-16 15:36:37 +0530

    setup done

--- index.js
+++ index.js
@@ -2,21 +2,19 @@ let arr = [13, 5, 5, 9, 45, 45, 98];
 
 let result = [];
 
-let seen=new Set()
 
-for (let i of arr) {
-  
-    if(!seen.has(i)){
-        seen.add(i)
+
+for(let i=0;i<arr.length;i++){
+    for(let j=0;j<arr.length-1-i;j++){
+if(arr[j]>arr[j+1]){
+    let temp=arr[j]
+    arr[j]=arr[j+1]
+    arr[j+1]=temp
+}
     }
 
 }
 
 
-for(let i of seen){
-    result.push(i)
-}
+console.log(arr)
 
-
-
-console.log(result);
