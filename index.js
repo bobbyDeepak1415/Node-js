5aa7c92029473c20189999e0fcfaafb083a71ac9
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-04-16 15:51:22 +0530

    setup done

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-let str = "AABKCCCDABJJKJ";
 
-let result = "";
 
-let obj = {};
 
-for (let i of str) {
-  obj[i] = (obj[i] || 0) + 1;
-}
-
-for(let key in obj){
-    result+=key+obj[key]
-}
-
-console.log(result)
