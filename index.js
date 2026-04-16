27b3916cd7092160b6955ddbc8060ee1183e0717
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-04-16 15:32:42 +0530

    setup done

--- index.js
+++ index.js
@@ -3,14 +3,11 @@ let arr=[13,5,5,9,45,45,98]
 
 let result=[]
 
-let seen=new Set()
 
 for(let i of arr){
-    if(!seen.has(i)){
-        seen.add(i)
+    if(!result.includes(i)){
+        result.push(i)
     }
 }
 
-
-
-console.log(seen)
+console.log(result)
